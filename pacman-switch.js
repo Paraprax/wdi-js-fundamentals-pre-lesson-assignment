1d6014adf2b984768f830b47f108bf729de59147
Paraprax <parkerjohnston28@gmail.com>
2017-04-26 01:54:01 -0400

    coding pacman switch file

---
+++ pacman-switch.js
@@ -0,0 +1,26 @@
+var ghost1 = 'Inky';
+var ghost2 = 'Blinky';
+var ghost3 = 'Pinky';
+var ghost4 = 'Clyde';
+var colour;
+
+switch (ghost1, ghost2, ghost3, ghost4) {
+  case 'Inky':
+    colour = 'Cyan';
+    break;
+  case 'Blinky':
+    colour = 'Red';
+    break;
+  case 'Pinky':
+    colour = 'Pink';
+    break;
+  case 'Clyde':
+    colour = 'Orange';
+    break;
+
+}
+
+console.log(ghost1 + ' is the colour ' + colour);
+console.log(ghost2 + ' is the colour ' + colour);
+console.log(ghost3 + ' is the colour ' + colour);
+console.log(ghost4 + ' is the colour ' + colour);
