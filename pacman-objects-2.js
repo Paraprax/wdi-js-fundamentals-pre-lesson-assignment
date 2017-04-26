a6e20d07d3372aa5d11191e09e178175ad7c0a81
Paraprax <parkerjohnston28@gmail.com>
2017-04-26 01:34:31 -0400

    creating arrarys of ghosts as objects and returning values

---
+++ pacman-objects-2.js
@@ -0,0 +1,10 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+
+//dot-notation
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+
+//bracket-notation
+console.log(ghosts['pinky']);
+console.log(ghosts['clyde']);
