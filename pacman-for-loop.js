bd36f4d6527f4ab37f80ff4846e3a0c4e3606b96
Paraprax <parkerjohnston28@gmail.com>
2017-04-26 01:45:32 -0400

    creating pacman if-else file

---
+++ pacman-for-loop.js
@@ -0,0 +1,10 @@
+ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+
+//for variable index = 0, when index is smaller than the # of ghosts in the array, increment the index by 1. Pac-Man eats each ghost in the index
+for (var index = 0; index < ghosts.length; index++) {
+  console.log('Pac-Man eats ' + ghosts[index]);
+}
+
+// index++ notation increments the value of index by 1
+// use index-- to decrement (reduce) the value by 1.
